c8a2a5060c38d6dc53189793461efd256224765e
Talismar Fernandes Costa <talismar788.una@gmail.com>
2022-03-09 20:24:38 -0300

    update finally

--- import_txt.py
+++
@@ -1,5 +0,0 @@
-from os import system
-
-"Importando os dados"
-with open("D:\\Algoritmos\\DisciplinaAlgoritmo\\ProcessamentoString\\palavra.txt", 'r', encoding="utf-8") as data:
-     words = data.read().lower()
--- remove_accents.py
+++
@@ -1,7 +0,0 @@
-from unicodedata import normalize
-
-def remove_accents(input_str):
-    return normalize('NFKD', input_str).encode('ASCII','ignore').decode('ASCII')
-
-if __name__ == "__main__":
-    print(remove_accents('áàãâäåª'))
